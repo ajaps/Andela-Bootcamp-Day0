c6f67886e20b7bc7ff9491080e8432f74c8985c3
AjapsFranklin <framky007@yahoo.com>
2017-05-08 23:06:34 +0100

    Improves the efficiency of the code

--- App/index.js
+++ App/index.js
@@ -1,43 +1,43 @@
 const getPrimes = (integerValue) =>{
     if (typeof(integerValue) != "number"){
-        return "invalid input";
+      return "invalid input";
     }
     else if(integerValue ==0){
-        return [1];
+      return [1];
     }
     else if(integerValue < 2){
-        return [];
+      return [];
     }else if(integerValue == 2){
-        return [2];
+      return [2];
     }
     else if(integerValue%1 !=0){
-        return "invalid input";
+      return "invalid input";
     }
     else if(typeof(integerValue) == "number"){
-        let theArray =[];
-        theArray.length=integerValue;
-        theSquareRoot = Math.sqrt(integerValue);
-        for(counter=2; counter<theSquareRoot; counter++){
-            if (typeof(theArray[counter]) == "undefined"){
-                 let determinantValue=0;
-                 innerCounter=counter*counter +(determinantValue*counter);
-                 while(innerCounter < integerValue){
-                      theArray[innerCounter] = 0;
-                      innerCounter=counter*counter +(++determinantValue*counter);
-                 }
+      let theArray =[];
+      theArray.length=integerValue;
+      theSquareRoot = Math.sqrt(integerValue);
+      for(counter=2; counter<=theSquareRoot; counter++){
+        if (typeof(theArray[counter]) == "undefined"){
+          let determinantValue=0;
+            innerCounter=counter*counter +(determinantValue*counter);
+            while(innerCounter <=integerValue){
+              theArray[innerCounter] = 0;
+              innerCounter=counter*counter +(++determinantValue*counter);
             }
         }
-         var arrayResult=[2];
-         for(i=3; i<=integerValue; i++){
-              if(theArray[i]!=false)
-                arrayResult.push(i);
+      }
+      var arrayResult=[2];
+      for(i=3; i<=integerValue; i++){
+        if(theArray[i]!=false){
+          arrayResult.push(i);
         }
-     return arrayResult;
+      }
+    return arrayResult;
     }
 return "Invalid input";
 
 }
-
 
 module.exports = {
     myPrime:getPrimes
