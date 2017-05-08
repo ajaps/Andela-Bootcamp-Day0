428d052f0540c6c33ce7d6a15853f04e8372fed1
AjapsFranklin <framky007@yahoo.com>
2017-05-08 22:59:52 +0100

    Add implentation to get prime numbers

--- App/index.js
+++ App/index.js
@@ -13,7 +13,27 @@ const getPrimes = (integerValue) =>{
     else if(integerValue%1 !=0){
         return "invalid input";
     }
-    
+    else if(typeof(integerValue) == "number"){
+        let theArray =[];
+        theArray.length=integerValue;
+        theSquareRoot = Math.sqrt(integerValue);
+        for(counter=2; counter<theSquareRoot; counter++){
+            if (typeof(theArray[counter]) == "undefined"){
+                 let determinantValue=0;
+                 innerCounter=counter*counter +(determinantValue*counter);
+                 while(innerCounter < integerValue){
+                      theArray[innerCounter] = 0;
+                      innerCounter=counter*counter +(++determinantValue*counter);
+                 }
+            }
+        }
+         var arrayResult=[2];
+         for(i=3; i<=integerValue; i++){
+              if(theArray[i]!=false)
+                arrayResult.push(i);
+        }
+     return arrayResult;
+    }
 return "Invalid input";
 
 }
