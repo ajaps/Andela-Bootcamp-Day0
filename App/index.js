f8c341af118aa04319372fdefa22b55030bca63e
AjapsFranklin <framky007@yahoo.com>
2017-05-08 22:57:48 +0100

    Adds implementation to accomodate edge cases

--- App/index.js
+++ App/index.js
@@ -1,5 +1,20 @@
 const getPrimes = (integerValue) =>{
-
+    if (typeof(integerValue) != "number"){
+        return "invalid input";
+    }
+    else if(integerValue ==0){
+        return [1];
+    }
+    else if(integerValue < 2){
+        return [];
+    }else if(integerValue == 2){
+        return [2];
+    }
+    else if(integerValue%1 !=0){
+        return "invalid input";
+    }
+    
+return "Invalid input";
 
 }
 
